ef5cb7bafb20c21106346dd4afedbf6873b939cc
Felipe Besson <flpbesson@gmail.com>
2016-05-18 20:08:05 -0300

    Implementando modelo Atracao

--- scrapy_cultural_sp/scrapy_cultural_sp/items.py
+++ scrapy_cultural_sp/scrapy_cultural_sp/items.py
@@ -1,14 +1,9 @@
 # -*- coding: utf-8 -*-
+from scrapy.item import Item, Field
 
-# Define here the models for your scraped items
-#
-# See documentation in:
-# http://doc.scrapy.org/en/latest/topics/items.html
-
-import scrapy
-
-
-class ScrapyCulturalSpItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class Atracao(Item):
+    cidade = Field()
+    lugar = Field()
+    dia = Field()
+    hora = Field()
+    artista = Field()
