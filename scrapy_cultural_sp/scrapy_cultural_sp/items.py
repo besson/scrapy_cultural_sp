d1f25d329b94845e728b8bc39fc397e538e0d26b
Felipe Besson <flpbesson@gmail.com>
2016-05-18 20:48:13 -0300

    Adicionando endereco

--- scrapy_cultural_sp/scrapy_cultural_sp/items.py
+++ scrapy_cultural_sp/scrapy_cultural_sp/items.py
@@ -3,7 +3,7 @@ from scrapy.item import Item, Field
 
 class Atracao(Item):
     cidade = Field()
-    lugar = Field()
+    endereco = Field()
     dia = Field()
     hora = Field()
     artista = Field()
