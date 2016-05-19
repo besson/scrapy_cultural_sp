ad39ff6f8409c52c7facd947f04acb095f8de69d
Felipe Besson <flpbesson@gmail.com>
2016-05-18 22:17:49 -0300

    Implementando segundo callback

--- scrapy_cultural_sp/scrapy_cultural_sp/spiders/virada_cultural_spider.py
+++ scrapy_cultural_sp/scrapy_cultural_sp/spiders/virada_cultural_spider.py
@@ -1,8 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from scrapy.selector import Selector
 from scrapy.http import Request
 from scrapy_cultural_sp.items import Atracao
 from scrapy.contrib.spiders import CrawlSpider
-
+from datetime import date
+from datetime import datetime
+import unicodedata
 
 class ViradaCulturalSpider(CrawlSpider):
     name = "virada_cultural"
@@ -14,3 +19,33 @@ class ViradaCulturalSpider(CrawlSpider):
 
         for url in urls_cidade:
             yield Request(url, self.parse_atracao)
+
+    def parse_atracao(self, response):
+        body_sel = Selector(response)
+
+        cidade = self.to_str(body_sel.xpath("//h1/text()"))
+        endereco = self.to_str(body_sel.xpath("//span[@class='address']/text()"))
+
+        atracao_selectors = body_sel.xpath("//ul[@class='list-events']//li")
+
+        for sel in atracao_selectors:
+            atracao = Atracao(cidade=cidade, endereco=endereco)
+            atracao["dia"] = self.to_date(sel.xpath(".//span[@class='date']//text()"))
+            atracao["hora"] = self.to_str(sel.xpath(".//span[@class='hour']//text()"))
+            atracao["artista"] = self.to_str(sel.xpath(".//span[@class='artist']//text()"))
+
+            self.print_item(atracao)
+
+    def to_str(self, element):
+        return element.extract()[0].encode("utf-8")
+
+    def to_date(self, element):
+        dia, mes = self.to_str(element).split("/")
+        ano = date.today().year
+
+        return datetime(ano, int(mes), int(dia)).strftime("%d-%m-%Y")
+
+    def print_item(self, atracao):
+        for k, v in atracao.items():
+            print "%s: %s" % (k, v)
+        print("--------------------")
